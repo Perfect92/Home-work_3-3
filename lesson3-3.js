29a647902f17a3fd8ecbb5f08e5b24ce283b1a16
Perfect92 <bakytbekovnurzanbek@gmail.com>
2023-01-03 10:05:30 +0300

    foot-site

--- lesson3-3.js
+++ lesson3-3.js
@@ -8,7 +8,7 @@ const hideTabContent = () => {
     })
     tabs.forEach((item) => {
         item.classList.remove("tabheader__item_active")
-    })
+    }) 
 }
 
 const showTabContent = (i = 3) => {
@@ -35,21 +35,22 @@ tabsParent.addEventListener("click", (event) => {
     }
 })
 
-let imgSlider = 0;
+let currSlide = 0;
 setInterval(() => {
-    if (imgSlider < 3) {
-        imgSlider++
+    if (currSlide <= 3) {
         hideTabContent()
-        showTabContent(imgSlider)
+        showTabContent(currSlide)
+        currSlide++
     } else {
-        imgSlider = 0
+        currSlide = 0
         hideTabContent()
-        showTabContent(imgSlider)
+        showTabContent(currSlide)
     }
-},1000);
+},2000);
 
 const modal = document.querySelector(".modal")
 const modalTrigger = document.querySelector(".btn_white")
+const modalTrigger2 = document.querySelector(".btn_dark")
 const closeModalBtn = document.querySelector(".modal__close")
 
 const openModal = () => {
@@ -58,25 +59,72 @@ const openModal = () => {
     document.body.style.overflow = "hidden"
 
 }
-
 modalTrigger.addEventListener("click", openModal)
+modalTrigger2.addEventListener("click", openModal)
+
 
 const closeModal = () => {
     modal.classList.add("hide")
     modal.classList.remove("show")
     document.body.style.overflow = ""
 }
-
 let modalOpened = false
 window.onscroll = () => {
-    if (document.documentElement.scrollTop >= 4000 && modalOpened === false) {
+    if (document.documentElement.scrollTop >= 3400 && modalOpened === false) {
         modalOpened = true
         openModal();
     }
 }
-
 document.body.addEventListener('click', (e) => {
    if (e.target.classList.contains('show')) {
        closeModal();
    }
 });
+
+const message = {
+    loading: "Идет загрузка...",
+    success:"Спасибо, скоро свяжемся",
+    fail:"Что-то пошло не так"
+}
+
+const forms = document.querySelectorAll("form")
+
+const postData = (form) => {
+    form.addEventListener("submit", (e) =>{
+        e.preventDefault()
+        setTimeout(()=>{
+            alert(message.loading)
+        }),1000
+        const request = new XMLHttpRequest()
+        request.open("POST","server.php")
+        request.setRequestHeader("Content-type","application/json")
+
+        const formData = new FormData(form)
+        const object ={}
+
+        formData.forEach((item,i) => {
+            const arr = [item,i]
+            console.log(arr)
+            object[i] = item
+        })
+        console.log(object)
+        const json = JSON.stringify(object)
+        request.send(json)
+        request.addEventListener("load", ()=>{
+            if (request.status === 200) {
+                console.log("ok")
+                setTimeout(()=>{
+                    alert(message.success)
+                }),1000
+            }else {
+                console.log("not ok")
+                setTimeout(()=>{
+                    alert(message.fail)
+                }),1000
+            }
+        })
+    })
+}
+forms.forEach((item) => {
+    postData(item)
+})
